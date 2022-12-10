578534588b8f08f9fbc6614bdc604043db12ca5a
WineRBK <qqty36@yandex.ru>
2022-12-10 20:38:08 +0700

    yandex-metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import Router from "next/router";
 import React from "react";
-
+import ym from "react-yandex-metrika";
+import { YMInitializer } from "react-yandex-metrika";
 import { AppProps } from "next/dist/shared/lib/router/router";
+
+Router.events.on("routeChangeComplete", (url: string) => {
+  if (typeof window !== "undefined") {
+    ym("hit", url);
+  }
+});
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return (
@@ -22,7 +30,11 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
         />
         <meta property="og:locale" content="ru_RU" />
       </Head>
-
+      <YMInitializer
+        accounts={[]}
+        options={{ webvisor: true, defer: true }}
+        version="2"
+      />
       <Component {...pageProps} />
     </>
   );
